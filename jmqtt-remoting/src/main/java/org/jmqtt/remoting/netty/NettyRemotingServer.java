7804f0666b7c626a2a8eb71b7ecb820213284d6d
blackcat <18200135187@163.com>
2018-11-19 23:18:21 +0800

    netty remoting

---
+++ jmqtt-remoting/src/main/java/org/jmqtt/remoting/netty/NettyRemotingServer.java
@@ -0,0 +1,119 @@
+package org.jmqtt.remoting.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.*;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.mqtt.MqttDecoder;
+import io.netty.handler.codec.mqtt.MqttEncoder;
+import io.netty.handler.codec.mqtt.MqttMessage;
+import io.netty.handler.timeout.IdleStateHandler;
+import org.jmqtt.common.config.NettyConfig;
+import org.jmqtt.common.helper.ThreadFactoryImpl;
+import org.jmqtt.common.log.LoggerName;
+import org.jmqtt.remoting.RemotingServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NettyRemotingServer implements RemotingServer {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggerName.REMOTING);
+    private NettyConfig nettyConfig;
+    private EventLoopGroup selectorGroup;
+    private EventLoopGroup ioGroup;
+    private Class<? extends ServerChannel> clazz;
+
+    public NettyRemotingServer(NettyConfig nettyConfig) {
+        this.nettyConfig = nettyConfig;
+
+        if(!nettyConfig.isUseEpoll()){
+            selectorGroup = new NioEventLoopGroup(nettyConfig.getIoThreadNum(),
+                    new ThreadFactoryImpl("SelectorEventGroup"));
+            ioGroup = new NioEventLoopGroup(nettyConfig.getIoThreadNum(),
+                    new ThreadFactoryImpl("IOEventGroup"));
+            clazz = NioServerSocketChannel.class;
+        }else{
+            selectorGroup = new EpollEventLoopGroup(nettyConfig.getIoThreadNum(),
+                    new ThreadFactoryImpl("SelectorEventGroup"));
+            ioGroup = new EpollEventLoopGroup(nettyConfig.getIoThreadNum(),
+                    new ThreadFactoryImpl("IOEventGroup"));
+            clazz = EpollServerSocketChannel.class;
+        }
+
+
+    }
+
+
+    @Override
+    public void start() {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(selectorGroup)
+                .group(ioGroup)
+                .channel(clazz)
+                .option(ChannelOption.SO_BACKLOG, nettyConfig.getTcpBackLog())
+                .option(ChannelOption.TCP_NODELAY, nettyConfig.isTcpNoDelay())
+                .option(ChannelOption.SO_SNDBUF, nettyConfig.getTcpSndBuf())
+                .option(ChannelOption.SO_RCVBUF, nettyConfig.getTcpRcvBuf())
+                .option(ChannelOption.SO_REUSEADDR, nettyConfig.isTcpReuseAddr())
+                .childOption(ChannelOption.SO_KEEPALIVE, nettyConfig.isTcpKeepAlive())
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast("mqttEncoder", MqttEncoder.INSTANCE)
+                                .addLast("mqttDecoder", new MqttDecoder(nettyConfig.getMaxMsgSize()))
+                                .addLast("idleStateHandler", new IdleStateHandler(0, 0, 60))
+                                .addLast("nettyConnectionManager", new NettyConnectManager())
+                                .addLast("nettyMqttHandler", new NettyMqttHandler());
+                    }
+                });
+        if(nettyConfig.isPooledByteBufAllocatorEnable()){
+            bootstrap.childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+        }
+
+        try {
+            ChannelFuture future = bootstrap.bind(nettyConfig.getTcpPort()).sync();
+            future.channel().closeFuture().sync();
+        }catch (InterruptedException ex){
+            log.error("Start tcp server failure.cause={}",ex);
+        }
+    }
+
+
+    @Override
+    public void shutdown() {
+        if (selectorGroup != null) {
+            selectorGroup.shutdownGracefully();
+        }
+        if (ioGroup != null) {
+            ioGroup.shutdownGracefully();
+        }
+    }
+
+    class NettyMqttHandler extends SimpleChannelInboundHandler<MqttMessage> {
+
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, MqttMessage mqttMessage) throws Exception {
+
+        }
+
+    }
+
+    class NettyConnectManager extends ChannelDuplexHandler {
+
+        @Override
+        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+            super.userEventTriggered(ctx, evt);
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            super.exceptionCaught(ctx, cause);
+        }
+    }
+}
