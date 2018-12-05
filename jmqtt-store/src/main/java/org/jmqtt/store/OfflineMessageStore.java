f4bfec11b3f63a2bcce5fe125060d9664afd9c94
blackcat <18200135187@163.com>
2018-12-05 22:13:36 +0800

    retain消息开发完成，开发离线消息功能

--- jmqtt-store/src/main/java/org/jmqtt/store/OfflineMessageStore.java
+++ jmqtt-store/src/main/java/org/jmqtt/store/OfflineMessageStore.java
@@ -1,8 +1,20 @@
 package org.jmqtt.store;
 
+import org.jmqtt.common.bean.Message;
+
+import java.util.concurrent.BlockingQueue;
+
 /**
- * 存储离线消息
+ * cleansession message
  */
 public interface OfflineMessageStore {
+
+    void initOfflneMsgCache(String clientId,int size);
+
+    void clearOfflineMsgCache(String clientId,int size);
+
+    boolean addOfflineMessage(String clientId, Message message);
+
+    BlockingQueue<Message> getAllOfflineMessage(String clientId);
 
 }
