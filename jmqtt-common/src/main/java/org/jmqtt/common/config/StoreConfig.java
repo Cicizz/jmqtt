b55e18c88a0f68755e41679099bf88fdb3d4d110
blackcat <18200135187@163.com>
2019-01-03 00:36:07 +0800

    序列化类

--- jmqtt-common/src/main/java/org/jmqtt/common/config/StoreConfig.java
+++ jmqtt-common/src/main/java/org/jmqtt/common/config/StoreConfig.java
@@ -2,9 +2,16 @@ package org.jmqtt.common.config;
 
 public class StoreConfig {
 
-    /* luffydb store configuration start */
-    private String luffyDBPath;
+    /* rocksdb store configuration start */
+    private String rocksDbPath = "rocksdb.db";
 
-    /* luffydb store configuration end */
+    /* rocksdb store configuration end */
 
+    public String getRocksDbPath() {
+        return rocksDbPath;
+    }
+
+    public void setRocksDbPath(String rocksDbPath) {
+        this.rocksDbPath = rocksDbPath;
+    }
 }
