9101b78ca540a4ddf679cc9ba8a8f724514862e0
张大伟 <p_dawzhang@tencent.com>
2019-05-28 18:07:13 +0800

    添加jstat远程监控

--- jmqtt-distribution/bin/runbroker.sh
+++ jmqtt-distribution/bin/runbroker.sh
@@ -21,7 +21,7 @@ export CLASSPATH=.:${BASE_DIR}/conf:${CLASSPATH}
 #===========================================================================================
 # JVM Configuration
 #===========================================================================================
-JAVA_OPT="${JAVA_OPT} -server -Xms128m -Xmx128m -Xmn64m"
+JAVA_OPT="${JAVA_OPT} -server -Xms1280m -Xmx1280m -Xmn640m"
 JAVA_OPT="${JAVA_OPT} -XX:+UseG1GC -XX:G1HeapRegionSize=16m -XX:G1ReservePercent=25 -XX:InitiatingHeapOccupancyPercent=30 -XX:SoftRefLRUPolicyMSPerMB=0 -XX:SurvivorRatio=8"
 JAVA_OPT="${JAVA_OPT} -verbose:gc -Xloggc:/dev/shm/mq_gc_%p.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintAdaptiveSizePolicy"
 JAVA_OPT="${JAVA_OPT} -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=30m"
@@ -30,6 +30,11 @@ JAVA_OPT="${JAVA_OPT} -XX:+AlwaysPreTouch"
 JAVA_OPT="${JAVA_OPT} -XX:MaxDirectMemorySize=15g"
 JAVA_OPT="${JAVA_OPT} -XX:-UseLargePages -XX:-UseBiasedLocking"
 JAVA_OPT="${JAVA_OPT} -Djava.ext.dirs=${JAVA_HOME}/jre/lib/ext:${BASE_DIR}/lib"
+
+JAVA_OPT="${JAVA_OPT} -Dcom.sun.management.jmxremote.ssl=false"
+JAVA_OPT="${JAVA_OPT} -Dcom.sun.management.jmxremote.authenticate=false"
+JAVA_OPT="${JAVA_OPT} -Dcom.sun.management.jmxremote.port=1099"
+
 #JAVA_OPT="${JAVA_OPT} -Xdebug -Xrunjdwp:transport=dt_socket,address=9555,server=y,suspend=n"
 JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
 JAVA_OPT="${JAVA_OPT} -cp ${CLASSPATH}"
