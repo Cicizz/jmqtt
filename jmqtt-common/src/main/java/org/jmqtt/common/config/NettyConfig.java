f33d999e820a7168924d36ef56d3176a4d7f7198
BEWINDOWEB <bewindoweb1995@foxmail.com>
2019-06-30 18:23:29 +0800

    support ssl
    support ssl

--- jmqtt-common/src/main/java/org/jmqtt/common/config/NettyConfig.java
+++ jmqtt-common/src/main/java/org/jmqtt/common/config/NettyConfig.java
@@ -13,16 +13,40 @@ public class NettyConfig {
     private boolean useEpoll = false;
     private boolean pooledByteBufAllocatorEnable = false;
 
+
     /**
      * tcp port default 1883
      */
+    private boolean startTcp = true;
     private int tcpPort = 1883;
 
-    private boolean startWebsocket = true;
     /**
      * websocket port default 1884
      */
+    private boolean startWebsocket = true;
     private int websocketPort = 1884;
+
+    /**
+     * tcp port with ssl default 8883
+     */
+    private boolean startSslTcp = true;
+    private int SslTcpPort = 8883;
+
+    /**
+     * websocket port with ssl default 8884
+     */
+    private boolean startSslWebsocket = true;
+    private int SslWebsocketPort = 8884;
+
+    /**
+     * SSL setting
+     */
+    private boolean useClientCA = false;
+    private String sslKeyStoreType = "PKCS12";
+    private String sslKeyFilePath = "/conf/server.pfx";
+    private String sslManagerPwd = "654321";
+    private String sslStorePwd = "654321";
+
 
     /**
      * max mqtt message size
@@ -141,5 +165,85 @@ public class NettyConfig {
         this.websocketPort = websocketPort;
     }
 
+    public boolean isStartTcp() {
+        return startTcp;
+    }
 
+    public void setStartTcp(boolean startTcp) {
+        this.startTcp = startTcp;
+    }
+
+
+    public boolean isStartSslTcp() {
+        return startSslTcp;
+    }
+
+    public void setStartSslTcp(boolean startSslTcp) {
+        this.startSslTcp = startSslTcp;
+    }
+
+
+    public int getSslTcpPort() {
+        return SslTcpPort;
+    }
+
+    public void setSslTcpPort(int sslTcpPort) {
+        SslTcpPort = sslTcpPort;
+    }
+
+    public boolean isStartSslWebsocket() {
+        return startSslWebsocket;
+    }
+
+    public void setStartSslWebsocket(boolean startSslWebsocket) {
+        this.startSslWebsocket = startSslWebsocket;
+    }
+
+    public int getSslWebsocketPort() {
+        return SslWebsocketPort;
+    }
+
+    public void setSslWebsocketPort(int sslWebsocketPort) {
+        SslWebsocketPort = sslWebsocketPort;
+    }
+
+    public boolean isUseClientCA() {
+        return useClientCA;
+    }
+
+    public void setUseClientCA(boolean useClientCA) {
+        this.useClientCA = useClientCA;
+    }
+
+    public String getSslKeyStoreType() {
+        return sslKeyStoreType;
+    }
+
+    public void setSslKeyStoreType(String sslKeyStoreType) {
+        this.sslKeyStoreType = sslKeyStoreType;
+    }
+
+    public String getSslKeyFilePath() {
+        return sslKeyFilePath;
+    }
+
+    public void setSslKeyFilePath(String sslKeyFilePath) {
+        this.sslKeyFilePath = sslKeyFilePath;
+    }
+
+    public String getSslManagerPwd() {
+        return sslManagerPwd;
+    }
+
+    public void setSslManagerPwd(String sslManagerPwd) {
+        this.sslManagerPwd = sslManagerPwd;
+    }
+
+    public String getSslStorePwd() {
+        return sslStorePwd;
+    }
+
+    public void setSslStorePwd(String sslStorePwd) {
+        this.sslStorePwd = sslStorePwd;
+    }
 }
