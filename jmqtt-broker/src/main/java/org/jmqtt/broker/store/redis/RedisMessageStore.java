7f80cdd34040cb43b80f0fc6fb3d7a124c6f8a46
stone99 <kjfupeng98@gmail.com>
2021-02-06 17:53:22 +0800

    chore:implements redis message store

--- jmqtt-broker/src/main/java/org/jmqtt/broker/store/redis/RedisMessageStore.java
+++ jmqtt-broker/src/main/java/org/jmqtt/broker/store/redis/RedisMessageStore.java
@@ -1,12 +1,17 @@
 package org.jmqtt.broker.store.redis;
 
+import com.alibaba.fastjson.JSONObject;
 import org.jmqtt.broker.common.config.BrokerConfig;
 import org.jmqtt.broker.common.model.Message;
 import org.jmqtt.broker.store.MessageStore;
+import org.jmqtt.broker.store.redis.support.RedisKeySupport;
+import org.jmqtt.broker.store.redis.support.RedisReplyUtils;
 import org.jmqtt.broker.store.redis.support.RedisSupport;
 import org.jmqtt.broker.store.redis.support.RedisUtils;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RedisMessageStore implements MessageStore {
 
@@ -23,34 +28,35 @@ public class RedisMessageStore implements MessageStore {
 
     @Override
     public boolean storeWillMessage(String clientId, Message message) {
-        return false;
+        return RedisReplyUtils.isOk(redisSupport.operate(jedis -> jedis.set(RedisKeySupport.WILL+clientId, JSONObject.toJSONString(message))));
     }
 
     @Override
     public boolean clearWillMessage(String clientId) {
-
-        return false;
+        redisSupport.operate(jedis -> jedis.del(RedisKeySupport.WILL+clientId));
+        return true;
     }
 
     @Override
     public Message getWillMessage(String clientId) {
-        return null;
+        return JSONObject.parseObject(redisSupport.operate(jedis -> jedis.get(RedisKeySupport.WILL+clientId)),Message.class);
     }
 
     @Override
     public boolean storeRetainMessage(String topic, Message message) {
-
-        return false;
+        redisSupport.operate(jedis -> jedis.hset(RedisKeySupport.RETAIN,String.valueOf(topic),JSONObject.toJSONString(message)));
+        return true;
     }
 
     @Override
     public boolean clearRetaionMessage(String topic) {
-
-        return false;
+        redisSupport.operate(jedis -> jedis.hdel(RedisKeySupport.RETAIN,topic));
+        return true;
     }
 
     @Override
     public Collection<Message> getAllRetainMsg() {
-        return null;
+        Map<String,String> retainMessages = redisSupport.operate(jedis -> jedis.hgetAll(RedisKeySupport.RETAIN));
+        return retainMessages.values().stream().map(val->JSONObject.parseObject(val,Message.class)).collect(Collectors.toList());
     }
 }
