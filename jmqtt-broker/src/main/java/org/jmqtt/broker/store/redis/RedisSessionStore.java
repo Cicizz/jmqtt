7352e605db7f3ae61adf5fd8b7ff0030a9764098
stone99 <kjfupeng98@gmail.com>
2021-02-06 17:05:04 +0800

    chore:implements redis session store

--- jmqtt-broker/src/main/java/org/jmqtt/broker/store/redis/RedisSessionStore.java
+++ jmqtt-broker/src/main/java/org/jmqtt/broker/store/redis/RedisSessionStore.java
@@ -1,16 +1,21 @@
 package org.jmqtt.broker.store.redis;
 
+import com.alibaba.fastjson.JSONObject;
 import org.jmqtt.broker.common.config.BrokerConfig;
 import org.jmqtt.broker.common.model.Message;
 import org.jmqtt.broker.common.model.Subscription;
 import org.jmqtt.broker.store.SessionState;
 import org.jmqtt.broker.store.SessionStore;
+import org.jmqtt.broker.store.redis.support.RedisKeySupport;
+import org.jmqtt.broker.store.redis.support.RedisReplyUtils;
 import org.jmqtt.broker.store.redis.support.RedisSupport;
 import org.jmqtt.broker.store.redis.support.RedisUtils;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RedisSessionStore implements SessionStore {
 
@@ -28,96 +33,105 @@ public class RedisSessionStore implements SessionStore {
 
     @Override
     public SessionState getSession(String clientId) {
-        return null;
+        return redisSupport.operate(jedis -> JSONObject.parseObject(jedis.get(RedisKeySupport.SESSION + clientId), SessionState.class));
     }
 
     @Override
     public boolean storeSession(String clientId, SessionState sessionState, boolean notifyClearOtherSession) {
-        return false;
-    }
-
-    @Override
-    public void clearSession(String clientId, boolean clearOfflineMsg) {
-
+        return RedisReplyUtils.isOk(redisSupport.operate(jedis -> jedis.set(RedisKeySupport.SESSION + clientId, JSONObject.toJSONString(sessionState))));
     }
 
     @Override
     public boolean storeSubscription(String clientId, Subscription subscription) {
-        return false;
+        redisSupport.operate(jedis -> jedis.hset(RedisKeySupport.SUBSCRIPTION + clientId, subscription.getTopic(), JSONObject.toJSONString(subscription)));
+        return true;
     }
 
     @Override
     public boolean delSubscription(String clientId, String topic) {
-        return false;
+        redisSupport.operate(jedis -> jedis.hdel(RedisKeySupport.SUBSCRIPTION, topic));
+        return true;
     }
 
     @Override
     public boolean clearSubscription(String clientId) {
-        return false;
+        redisSupport.operate(jedis -> jedis.del(RedisKeySupport.SUBSCRIPTION + clientId));
+        return true;
     }
 
     @Override
     public Set<Subscription> getSubscriptions(String clientId) {
-        return null;
+        Map<String, String> subscriptions = redisSupport.operate(jedis -> jedis.hgetAll(RedisKeySupport.SUBSCRIPTION + clientId));
+        return subscriptions.values().stream().map(val -> JSONObject.parseObject(val, Subscription.class)).collect(Collectors.toSet());
     }
 
     @Override
     public boolean cacheInflowMsg(String clientId, Message message) {
-        return false;
+        redisSupport.operate(jedis -> jedis.hset(RedisKeySupport.REC_FLOW_MESSAGE + clientId, String.valueOf(message.getMsgId()), JSONObject.toJSONString(message)));
+        return true;
     }
 
     @Override
     public Message releaseInflowMsg(String clientId, int msgId) {
-        return null;
+        return JSONObject.parseObject(redisSupport.operate(jedis -> jedis.hdel(RedisKeySupport.REC_FLOW_MESSAGE, String.valueOf(msgId))), Message.class);
     }
 
     @Override
     public Collection<Message> getAllInflowMsg(String clientId) {
-        return null;
+        Map<String, String> inflowMessages = redisSupport.operate(jedis -> jedis.hgetAll(RedisKeySupport.REC_FLOW_MESSAGE + clientId));
+        return inflowMessages.values().stream().map(val -> JSONObject.parseObject(val, Message.class)).collect(Collectors.toSet());
     }
 
     @Override
     public boolean cacheOutflowMsg(String clientId, Message message) {
-        return false;
+        redisSupport.operate(jedis -> jedis.hset(RedisKeySupport.SEND_FLOW_MESSAGE + clientId, String.valueOf(message.getMsgId()), JSONObject.toJSONString(message)));
+        return true;
     }
 
     @Override
     public Collection<Message> getAllOutflowMsg(String clientId) {
-        return null;
+        Map<String, String> outflowMessages = redisSupport.operate(jedis -> jedis.hgetAll(RedisKeySupport.SEND_FLOW_MESSAGE + clientId));
+        return outflowMessages.values().stream().map(val -> JSONObject.parseObject(val, Message.class)).collect(Collectors.toSet());
     }
 
     @Override
     public Message releaseOutflowMsg(String clientId, int msgId) {
-        return null;
+        return JSONObject.parseObject(redisSupport.operate(jedis -> jedis.hdel(RedisKeySupport.SEND_FLOW_MESSAGE + clientId, String.valueOf(msgId))), Message.class);
     }
 
     @Override
     public boolean cacheOutflowSecMsgId(String clientId, int msgId) {
-        return false;
+        String value = String.valueOf(msgId);
+        redisSupport.operate(jedis -> jedis.hset(RedisKeySupport.SEND_FLOW_SEC_MESSAGE + clientId, value, value));
+        return true;
     }
 
     @Override
     public boolean releaseOutflowSecMsgId(String clientId, int msgId) {
-        return false;
+        redisSupport.operate(jedis -> jedis.hdel(RedisKeySupport.SEND_FLOW_SEC_MESSAGE + clientId, String.valueOf(msgId)));
+        return true;
     }
 
     @Override
     public List<Integer> getAllOutflowSecMsgId(String clientId) {
-        return null;
+        Map<String, String> secMsgIds = redisSupport.operate(jedis -> jedis.hgetAll(RedisKeySupport.SEND_FLOW_SEC_MESSAGE + clientId));
+        return secMsgIds.values().stream().map(Integer::valueOf).collect(Collectors.toList());
     }
 
     @Override
     public boolean storeOfflineMsg(String clientId, Message message) {
-        return false;
+        redisSupport.operate(jedis -> jedis.hset(RedisKeySupport.OFFLINE + clientId, String.valueOf(message.getMsgId()), JSONObject.toJSONString(message)));
+        return true;
     }
 
     @Override
     public Collection<Message> getAllOfflineMsg(String clientId) {
-        return null;
+        Map<String, String> offlineMessages = redisSupport.operate(jedis -> jedis.hgetAll(RedisKeySupport.OFFLINE + clientId));
+        return offlineMessages.values().stream().map(val -> JSONObject.parseObject(val, Message.class)).collect(Collectors.toSet());
     }
 
     @Override
     public boolean clearOfflineMsg(String clientId) {
-        return false;
+        return RedisReplyUtils.isOk(redisSupport.operate(jedis -> jedis.del(RedisKeySupport.OFFLINE + clientId)));
     }
 }
