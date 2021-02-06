b3c838b92f05e6c4328a800519a4ba644800a6cd
stone99 <kjfupeng98@gmail.com>
2021-02-06 23:09:35 +0800

    chore:implements redis cluster event handler

--- jmqtt-broker/src/main/java/org/jmqtt/broker/processor/dispatcher/redis/RedisClusterEventHandler.java
+++ jmqtt-broker/src/main/java/org/jmqtt/broker/processor/dispatcher/redis/RedisClusterEventHandler.java
@@ -1,16 +1,36 @@
 package org.jmqtt.broker.processor.dispatcher.redis;
 
+import com.alibaba.fastjson.JSONObject;
 import org.jmqtt.broker.common.config.BrokerConfig;
 import org.jmqtt.broker.processor.dispatcher.ClusterEventHandler;
 import org.jmqtt.broker.processor.dispatcher.EventConsumeHandler;
 import org.jmqtt.broker.processor.dispatcher.event.Event;
+import org.jmqtt.broker.store.redis.support.RedisKeySupport;
+import org.jmqtt.broker.store.redis.support.RedisSupport;
+import org.jmqtt.broker.store.redis.support.RedisUtils;
+import redis.clients.jedis.JedisPubSub;
 
 import java.util.List;
+import java.util.UUID;
 
 public class RedisClusterEventHandler implements ClusterEventHandler {
+    private static final String INSTANCE_CHANNEL_ID = RedisKeySupport.PREFIX + "_CLUSTER_CHANNEL_" + UUID.randomUUID();
+    private static final String INSTANCE_CHANNEL_PATTERN = RedisKeySupport.PREFIX + "_CLUSTER_CHANNEL_*";
+    private EventConsumeHandler eventConsumeHandler;
+    private RedisSupport redisSupport;
+
     @Override
     public void start(BrokerConfig brokerConfig) {
-
+        this.redisSupport = RedisUtils.getInstance().createSupport(brokerConfig);
+        this.redisSupport.operate(jedis -> {
+            jedis.psubscribe(new JedisPubSub() {
+                @Override
+                public void onMessage(String channel, String message) {
+                    eventConsumeHandler.consumeEvent(JSONObject.parseObject(message,Event.class));
+                }
+            }, INSTANCE_CHANNEL_PATTERN);
+            return true;
+        });
     }
 
     @Override
@@ -20,12 +40,13 @@ public class RedisClusterEventHandler implements ClusterEventHandler {
 
     @Override
     public boolean sendEvent(Event event) {
-        return false;
+        redisSupport.operate(jedis -> jedis.publish(INSTANCE_CHANNEL_ID,JSONObject.toJSONString(event)));
+        return true;
     }
 
     @Override
     public void setEventConsumeHandler(EventConsumeHandler eventConsumeHandler) {
-
+        this.eventConsumeHandler = eventConsumeHandler;
     }
 
     @Override
