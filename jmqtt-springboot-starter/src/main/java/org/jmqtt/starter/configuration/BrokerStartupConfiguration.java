3748875a88123c96fb63ae666598dd3f9d9445c5
dyc <duyichang@chinatelecom.com>
2021-02-27 12:24:39 +0800

    删除无用import

--- jmqtt-springboot-starter/src/main/java/org/jmqtt/starter/configuration/BrokerStartupConfiguration.java
+++ jmqtt-springboot-starter/src/main/java/org/jmqtt/starter/configuration/BrokerStartupConfiguration.java
@@ -6,7 +6,6 @@ import org.jmqtt.broker.common.config.NettyConfig;
 import org.jmqtt.starter.service.BrokerStartupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
